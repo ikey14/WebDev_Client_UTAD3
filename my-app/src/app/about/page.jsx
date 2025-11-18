14f54a3bee21715298bca7603c08def42710145c
Enrique Iglesias <ike.umafi@gmail.com>
2025-11-18 12:27:06 +0100

    Started learning tailwind

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,8 +1,17 @@
+"use client"
+
 import Image from "next/image";
+import {useRouter} from "next/navigation"
 
 export default function About()
 {
+  const router = useRouter();
+
   return(<>
     <h1>AYE BRUDDA DIS DE ABAUT SECTIAN</h1>
+    <button onClick = {() => {
+      alert("Executing");
+      router.push("/");
+    }}>Click Router</button>
   </>)
 }
---
+++ tailwind-testing/src/app/page.jsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (<div className = "blue-Black-White-GradBR w-full h-lvh mx-auto">
+    <div className = "flex items-center justify-center">
+      <h1 className = "text-3xl font-bold underline text-blue-600 mx-auto">A CORRER LO LAKERS</h1>
+      
+    </div>
+    <div className = "flex items-center justify-center">
+      <button className = "p-2 rounded-2xl text-red-600 hover:bg-amber-700/50 disabled:bg-gray-600" disabled>P-2</button>
+    </div>
+    
+
+    <div className = "w-fit rounded-2x1 mx-auto">
+      <h1 className = "mx-auto">AAAAAAAAAAA</h1>
+    </div>
+  </div>);
+}
